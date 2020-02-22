5536a567a5a5e25a2fea3d04e9ebc9a2d55d93fc
Oleksandr Priadko <oleksandpriadko@gmail.com>
2020-02-23 01:00:25 +1100

    - UI elements appearance - activity_main.xml with UI elements

---
+++ app/src/main/java/com/oleksandrpriadko/test/market/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.oleksandrpriadko.test.market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.view.menu.BaseMenuPresenter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
